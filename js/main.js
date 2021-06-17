ac83b864a832f55102e47b555c4bdbff86ede828
Daria Zhuravleva <71088792+dardaria@users.noreply.github.com>
2021-06-18 00:31:34 +0300

    project 5, added cards to elements section

--- js/main.js
+++ js/main.js
@@ -6,7 +6,35 @@ let profileOccupation = document.querySelector('.profile__description');
 let nameInput = document.querySelector('.edit-form__name');
 let occupationInput = document.querySelector('.edit-form__occupation');
 let editProfileForm = document.querySelector('.edit-form');
+const cardTemplate = document.querySelector('#card').content;
+const elements = document.querySelector('.elements');
 
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 
 function openPopup() {
     nameInput.value = profileName.textContent;
@@ -32,3 +60,13 @@ function handleSubmit(event) {
 editProfileButton.addEventListener('click', openPopup);
 closePopupButton.addEventListener('click', closePopup);
 editProfileForm.addEventListener('submit', handleSubmit);
+
+initialCards.forEach(function(card) {
+    const cardElement = cardTemplate.cloneNode(true);
+
+    cardElement.querySelector('.elements__pic-image').src = card.link;
+    cardElement.querySelector('.elements__pic-image').alt = card.name;
+    cardElement.querySelector('.elements__title').textContent = card.name;
+
+    elements.append(cardElement);
+});
